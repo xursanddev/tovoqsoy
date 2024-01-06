32db93cc426bb71d22bf3a517b7547a516522f07
Dilshod Fayzullayev <brogrammer.devs@gmail.com>
2024-01-06 15:13:53 +0500

    :lipstick: The user interface has been updated

--- src/main.js
+++ src/main.js
@@ -1,25 +1,88 @@
 import "halfmoon/css/halfmoon.min.css";
-import './style.css'
-import javascriptLogo from '/javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import './slider.css';
+// import './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const $ = selector => {
+    return document.querySelector(selector);
+};
 
-setupCounter(document.querySelector('#counter'))
+function next() {
+    if ($(".hide")) {
+        $(".hide").remove();
+    }
+
+    /* Step */
+
+    if ($(".prev")) {
+        $(".prev").classList.add("hide");
+        $(".prev").classList.remove("prev");
+    }
+
+    $(".act").classList.add("prev");
+    $(".act").classList.remove("act");
+
+    $(".next").classList.add("act");
+    $(".next").classList.remove("next");
+
+    /* New Next */
+
+    $(".new-next").classList.remove("new-next");
+
+    const addedEl = document.createElement('li');
+
+    $(".list").appendChild(addedEl);
+    addedEl.classList.add("next", "new-next");
+}
+
+function prev() {
+    $(".new-next").remove();
+
+    /* Step */
+
+    $(".next").classList.add("new-next");
+
+    $(".act").classList.add("next");
+    $(".act").classList.remove("act");
+
+    $(".prev").classList.add("act");
+    $(".prev").classList.remove("prev");
+
+    /* New Prev */
+
+    $(".hide").classList.add("prev");
+    $(".hide").classList.remove("hide");
+
+    const addedEl = document.createElement('li');
+
+    $(".list").insertBefore(addedEl, $(".list").firstChild);
+    addedEl.classList.add("hide");
+}
+
+const slide = element => {
+    /* Next slide */
+
+    if (element.classList.contains('next')) {
+        next();
+
+        /* Previous slide */
+
+    } else if (element.classList.contains('prev')) {
+        prev();
+    }
+}
+
+const slider = $(".list"),
+    swipe = new Hammer($(".swipe"));
+
+slider.onclick = event => {
+    slide(event.target);
+}
+
+swipe.on("swipeleft", (ev) => {
+    next();
+});
+
+swipe.on("swiperight", (ev) => {
+    prev();
+});
